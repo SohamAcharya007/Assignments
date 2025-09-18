20d39581f89fa1f6769474f9655d0ce407500696
SohamAcharya007 <sohamacharya007@gmail.com>
2025-09-18 20:39:52 +0530

    Add files via upload

---
+++ Day6/MultiplicationFor.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+  int i,n,mul;
+  printf("Enter a number: ");
+  scanf("%d",&n);
+  for(i=1;i<=10;i++){
+    mul=n*i;
+    printf("%d x %d = %d\n",n,i,mul);
+  }
+  return 0;
+}
